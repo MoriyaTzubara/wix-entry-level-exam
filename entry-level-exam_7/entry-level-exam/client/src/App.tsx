76ee17a58cc9e057ddbbd127f053f08eb345c275
MoriyaTzubara <m0533151327@gmail.com>
2021-03-02 03:19:03 +0200

    first task

--- entry-level-exam_7/entry-level-exam/client/src/App.tsx
+++ entry-level-exam_7/entry-level-exam/client/src/App.tsx
@@ -32,6 +32,10 @@ export class App extends React.PureComponent<{}, AppState> {
 		return (<ul className='tickets'>
 			{filteredTickets.map((ticket) => (<li key={ticket.id} className='ticket'>
 				<h5 className='title'>{ticket.title}</h5>
+				<div>
+					<p className='content'>{ticket.content}</p>
+					<a className='see-more'>See more</a>
+				</div>
 				<footer>
 					<div className='meta-data'>By {ticket.userEmail} | { new Date(ticket.creationTime).toLocaleString()}</div>
 				</footer>
